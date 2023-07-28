f5930846fbba57d6e8486087a601e36fb1d234c7
Amen Munir <amenmunir16@gmail.com>
2023-07-28 15:02:11 +0500

    amen

---
+++ challenges.js
@@ -0,0 +1,17 @@
+let mybool = true;  //challenge 1
+let myString = 'Hello World'; //challenge 2
+const firstNumber = 20; //challenge 3
+let secondNumber = 40; //challenge 4
+secondNumber = 80;  //challenge 5
+
+myArray = [mybool, myString]; //challenge 6
+
+myobject = {  //challenge 7
+    property1: myArray,
+    sumProperty:(firstNumber+secondNumber)
+
+
+}
+console.log(myobject); //challenge 8
+ console.log(myobject.sumProperty); //challenge 9
+console.log(myobject.property1[1]); //challenge 10
